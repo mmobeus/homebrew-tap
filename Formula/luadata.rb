3b71773dea4bf1e4ef62710f09d5db9b0c8a37cd
mmobeus-dev <mmobeus@mmobeus.com>
2026-03-21 11:09:27 -0600

    Add formula for luadata-cli

---
+++ Formula/luadata.rb
@@ -0,0 +1,24 @@
+class Luadata < Formula
+  desc "Parse Lua data files and convert to JSON"
+  homepage "https://github.com/mmobeus/luadata"
+  version "0.1.9"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/mmobeus/luadata/releases/download/v#{version}/luadata-darwin_arm64.tar.gz"
+      sha256 "PLACEHOLDER"
+    elsif Hardware::CPU.intel?
+      url "https://github.com/mmobeus/luadata/releases/download/v#{version}/luadata-darwin_amd64.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  def install
+    bin.install "luadata"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/luadata --version")
+  end
+end
